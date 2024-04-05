7c5c64a21d755daf3b3bb994eedcd4b14d3c2535
Apurva Anand <apurvanand29@gmail.com>
2024-04-05 17:30:07 -0500

    Adding Employee.java to show use of different constructors

---
+++ Constructor/src/Employee.java
@@ -0,0 +1,12 @@
+/**
+ * This Employee class will help you access employee records using
+ * different constructors
+ */
+public class Employee {
+    public static void main(String[] args) throws Exception {
+
+        // Object declaration
+        EmployeeDetails emp1, emp2, emp3;
+
+    }
+}
