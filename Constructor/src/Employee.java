432c42512cf1f7fcd3232750f4dbaafb705b123d
Apurva Anand <apurvanand29@gmail.com>
2024-04-05 17:30:49 -0500

    Added object for three employees

--- Constructor/src/Employee.java
+++ Constructor/src/Employee.java
@@ -8,5 +8,12 @@ public class Employee {
         // Object declaration
         EmployeeDetails emp1, emp2, emp3;
 
+        /**
+         * Object creation for employees: emp1,emp2,emp3
+         */
+        emp1 = new EmployeeDetails("Susan Meyers", 8647765, "IT", "Sr Programmer/Analyst");
+        emp2 = new EmployeeDetails("Ahmad Karim", 5424130);
+        emp3 = new EmployeeDetails();
+
     }
 }
