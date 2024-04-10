fdf5c7b41e7fdb9e0895aa42ffcf6b06b3e206ee
Apurva Anand <apurvanand29@gmail.com>
2024-04-10 12:22:13 -0500

    Using scanner to take user input

--- ArrayOperations/RandomResult.java
+++ ArrayOperations/RandomResult.java
@@ -1,11 +1,20 @@
 package ArrayOperations;
 
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class RandomResult {
     public static void main(String[] args) throws Exception {
         ArrayList<Integer> numberList = new ArrayList<>(200);
         int[] numberArray = new int[200];
+
+        // Taking input from file
+        Scanner sc = new Scanner(new FileReader("MOCK_DATA.txt"));
+        while (sc.hasNext()) {
+            numberList.add(sc.nextInt());
+        }
+        sc.close();
 
     }
 }
