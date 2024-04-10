1ed9253418deb483e62e460cfc355e4b2aedc970
Apurva Anand <apurvanand29@gmail.com>
2024-04-10 12:20:50 -0500

    Added main method to RandomPlatform

--- ArrayOperations/RandomResult.java
+++ ArrayOperations/RandomResult.java
@@ -1,5 +1,11 @@
 package ArrayOperations;
 
-public class RandomResult {
+import java.util.ArrayList;
 
+public class RandomResult {
+    public static void main(String[] args) throws Exception {
+        ArrayList<Integer> numberList = new ArrayList<>(200);
+        int[] numberArray = new int[200];
+
+    }
 }
