b45da17a9b12d9d9df09a893af5642f314693555
Apurva Anand <apurvanand29@gmail.com>
2024-05-16 23:27:19 -0500

    Bank class will use Savings Account for withdrawal and deposit to account with random numbers

---
+++ BankingOperations/src/Bank.java
@@ -0,0 +1,20 @@
+/* *********************************************************************
+*  +-+-+-+-+-+-+-+-+-+-+-+-+-+
+*   A P U R V A     A N A N D
+*  +-+-+-+-+-+-+-+-+-+-+-+-+-+
+* 
+* Description: This will help you understand the concept of overriding
+*              and abstract class through a banking application
+*
+***********************************************************************
+*/
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Bank {
+    public static void main(String[] args) throws Exception {
+        ArrayList<SavingsAccount> randAccount = new ArrayList<>();
+
+    }
+}
