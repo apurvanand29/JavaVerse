06cc7e8f1645232cf9ea0a6404ac6039f1607ba3
Apurva Anand <apurvanand29@gmail.com>
2024-05-16 23:28:54 -0500

    Using random class, balance and interest rate is getting generated

--- BankingOperations/src/Bank.java
+++ BankingOperations/src/Bank.java
@@ -16,5 +16,16 @@ public class Bank {
     public static void main(String[] args) throws Exception {
         ArrayList<SavingsAccount> randAccount = new ArrayList<>();
 
+        // To generate random values and store in SavingsAccount object
+        // Random class is being used to generate random numbers
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            double balance = rand.nextDouble(0, 50);
+            double annualInterestRate = rand.nextDouble(1, 15);
+
+            SavingsAccount account = new SavingsAccount(balance, annualInterestRate);
+            randAccount.add(account);
+        }
+
     }
 }
