49cc20804af19c99d1449cbf3bca37b5ffee820d
Apurva Anand <apurvanand29@gmail.com>
2024-05-16 23:44:47 -0500

    Added override to give status of account and deposit

--- BankingOperations/src/SavingsAccount.java
+++ BankingOperations/src/SavingsAccount.java
@@ -13,4 +13,24 @@ public class SavingsAccount extends BankAccount implements Comparable<BankAccoun
         status = super.getBalance() > 25;
     }
 
+    @Override
+    public void withdraw(double amount) {
+        if (super.getBalance() < amount)
+            System.exit(0);
+
+        if (super.getBalance() < 25) {
+            status = false;
+        } else {
+            super.withdraw(amount);
+        }
+    }
+
+    @Override
+    public void deposit(double amount) {
+        super.deposit(amount);
+        if (super.getBalance() > 25) {
+            status = true;
+        }
+    }
+
 }
